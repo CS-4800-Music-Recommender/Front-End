7fdb7b1bf7d3df222e9cae2d1d73a20b8fe95d1f
OJ101003 <hackerdude1010@gmail.com>
2023-11-19 02:19:32 -0800

    React icons installed

--- src/pages/PlaylistPage.jsx
+++ src/pages/PlaylistPage.jsx
@@ -1,14 +1,20 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
 import logo from "../images/splashscreen/bpm-logo.png";
+import { List } from "react-bootstrap-icons";
 
 const PlaylistPage = () => {
   return (
     <Container fluid className="p-5 text-center ">
-      <Row >
-        <Col></Col>
+      <Row>
         <Col>
+          <List size={70}/>
+        </Col>
+        <Col xs={8}>
           <Image src={logo} width={300} fluid />
         </Col>
+      </Row>
+      <Row>
+        
       </Row>
     </Container>
   );
