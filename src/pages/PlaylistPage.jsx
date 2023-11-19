9eecc116a0f1929fee7ef2698500af4fd2866e3f
OJ101003 <hackerdude1010@gmail.com>
2023-11-19 04:15:35 -0800

    Recommended songs listgroup

--- src/pages/PlaylistPage.jsx
+++ src/pages/PlaylistPage.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-bootstrap";
 import logo from "../images/splashscreen/bpm-logo.png";
 import { List } from "react-bootstrap-icons";
+import { ListGroup } from "react-bootstrap/esm";
 
 const PlaylistPage = () => {
   return (
@@ -22,50 +23,69 @@ const PlaylistPage = () => {
         </Col>
       </Row>
       <Row className="mx-5">
-        <div className="text-center" style={{ width: "15%" }}>
+        <Col xs={4}>
+          <div className="text-center" style={{ width: "50%" }}>
+            <Row>
+              <Button
+                variant="success"
+                to="/playlist"
+                className="my-3 p-3 fs-5 mx-5"
+              >
+                Export Playlist
+              </Button>
+            </Row>
+            <Row>
+              <Button
+                variant="success"
+                to="/playlist"
+                className="my-3 p-3 fs-5 mx-5"
+              >
+                Import Playlist
+              </Button>
+            </Row>
+
+            <Row>
+              <Button
+                variant="success"
+                to="/playlist"
+                className="my-3 p-3 fs-5 mx-5"
+              >
+                Create Playlist
+              </Button>
+            </Row>
+          </div>
           <Row>
-            <Button
-              variant="success"
-              to="/playlist"
-              className="my-3 p-3 fs-5 mx-5"
-            >
-              Export Playlist
-            </Button>
+            <InputGroup className="mb-3" style={{ width: "90%" }}>
+              <Form.Control
+                placeholder="Search Song or Artist"
+                aria-label="Search Song or Artist"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-secondary" id="button-addon2">
+                Artist
+              </Button>
+              <Button variant="outline-secondary" id="button-addon2">
+                Song
+              </Button>
+            </InputGroup>
           </Row>
-          <Row>
-            <Button
-              variant="success"
-              to="/playlist"
-              className="my-3 p-3 fs-5 mx-5"
-            >
-              Import Playlist
-            </Button>
-          </Row>
-          <Row>
-            <Button
-              variant="success"
-              to="/playlist"
-              className="my-3 p-3 fs-5 mx-5"
-            >
-              Create Playlist
-            </Button>
-          </Row>
-        </div>
-        <Row>
-          <InputGroup className="mb-3" style={{ width: "25%" }}>
-            <Form.Control
-              placeholder="Search Song or Artist"
-              aria-label="Search Song or Artist"
-              aria-describedby="basic-addon2"
-            />
-            <Button variant="outline-secondary" id="button-addon2">
-              Artist
-            </Button>
-            <Button variant="outline-secondary" id="button-addon2">
-              Song
-            </Button>
-          </InputGroup>
-        </Row>
+        </Col>
+        <Col>
+          <div className="text-center " style={{ width: "50%" }} id="playlist">
+            <h1>Recommended Songs</h1>
+            <ListGroup className="fs-5">
+              <ListGroup.Item action>
+                List Group Item 1
+              </ListGroup.Item>
+              <ListGroup.Item action>
+                List Group Item 2
+              </ListGroup.Item>
+              <ListGroup.Item action>
+                List Group Item 3
+              </ListGroup.Item>
+            </ListGroup>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
